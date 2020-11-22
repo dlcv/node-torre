94e0c6361688f2ac94cbfb67bce68e88ed7463ac
dlcv <dlcv@live.com.ve>
2020-11-22 11:13:07 -0400

    Change to env variable test

--- src/database.js
+++ src/database.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const { TORRE_REMOTE_MONGODB_USER, TORRE_REMOTE_MONGODB_PASS, TORRE_REMOTE_MONGODB_HOST, TORRE_REMOTE_MONGODB_NAME } = process.env;
 // const MONGODB_URI = `mongodb://${TORRE_MONGODB_HOST}/${TORRE_MONGODB_DATABASE}`;
 
-MONGODB_URI = `mongodb+srv://node-dlcv:CIm06RCF2ufOqwBb@cluster0.x8xwn.mongodb.net/torre-cloud?retryWrites=true&w=majority`
+// MONGODB_URI = `mongodb+srv://node-dlcv:CIm06RCF2ufOqwBb@cluster0.x8xwn.mongodb.net/torre-cloud?retryWrites=true&w=majority`
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useCreateIndex: true,
